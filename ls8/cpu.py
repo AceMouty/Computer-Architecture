29987048fb23bde496797e5a124ac8e29e34dea7
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-24 17:35:54 -0600

    feat: CPU has the ability to insert, print and halt execution
    Add in a register attr to cpu class
    added in a ram attr to the cpu class
    add in a program counter to the cpu class
    
    CPU can currently insert data to a register, read the ram attr, write to ram
    and also print out to the console and exit with a status code.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,24 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # create 8 registers
+        self.reg = [0] * 8
+        # create 256 bytes of memory
+        self.ram = [0] * 256
+        # create a program counter (pc)
+        self.pc = 0
+
+    def ram_read(self, addr):
+        return self.ram[addr]
+
+    def write_ram(self, addr, value):
+        self.ram[addr] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +30,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +59,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +73,37 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        # setup vars for simple op codes
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        # load the program into memory
+        self.load()
+
+        # var to type less out in the while loop
+        read_ram = self.ram_read
+
+        # run the program
+        while True:
+
+            # simple var to type less
+            pc = self.pc
+            # que an operation to start at default is 0
+            op = read_ram(pc)
+
+            if op == LDI:
+                self.reg[read_ram(pc + 1)] = read_ram(pc + 2)
+                self.pc += 3
+            elif op == PRN:
+                print(self.reg[read_ram(pc + 1)])
+                self.pc += 2
+            elif op == HLT:
+                sys.exit(1)
+            else:
+                print("ERR: UNKNOWN COMMAND:\t", op)
+
+
+c = CPU()
+c.run()
